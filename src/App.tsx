e9b05341ae4d4305bd6e15f930f4efc61628bf90
chika <hawmea@yahoo.co.jp>
2023-04-22 14:23:43 +0900

    emotionのインストールと、簡単なMUIのコードを設置テスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
 import "./App.css";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <p>hoge</p>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Create React App example in TypeScript
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
   );
 }
 

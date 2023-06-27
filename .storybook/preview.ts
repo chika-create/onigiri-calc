2e4eef25f1fe5bd12b95eed4fbb70b719d46863f
chika-create <yasui.chika@a-tm.co.jp>
2023-06-27 19:44:47 +0900

    test: 画面を作る_繋がれた画面・API をモックする

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,22 @@
-import type { Preview } from "@storybook/react";
+
 import '../src/index.css';
 
-const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+ // Registers the msw addon
+ import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+ // Initialize MSW
+ initialize();
+
+ // Provide the MSW addon decorator globally
+ export const decorators = [mswDecorator];
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
-
-export default preview;

259d540200af8c56f32ea7fefb9c8ffcb250e02c
chika-create <yasui.chika@a-tm.co.jp>
2023-07-05 20:07:08 +0900

    test: MinDeckNum のストーリーファイルを用意

--- src/components/parts/initialSetting/MinDeckNum.stories.tsx
+++ src/components/parts/initialSetting/MinDeckNum.stories.tsx
@@ -1,14 +1,24 @@
 // Storybook用の型インポート
 // import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 // カタログ化対象のコンポーネントインポート
+
+// import React, { useState, useContext } from "react";
+// import { useForm } from "react-hook-form";
+
 import MinDeckNum from './MinDeckNum'
+// import { TabPanelProps, CalcFormInput } from "../../../types";
+
+// const { register } = useForm<CalcFormInput>();
+const register = "hoge"
 
 export default {
   title: 'Button',
   component: MinDeckNum,
 };
 
-export const Desktop: Story = {}
+export const Desktop: Story = {
+  register: register
+}
 
 // 1: メタデータ記述
 // export default {

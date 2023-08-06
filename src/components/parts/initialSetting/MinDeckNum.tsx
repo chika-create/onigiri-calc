5d741472602bf6e733827fe4513c88dab8cb795b
chika <hawmea@yahoo.co.jp>
2023-08-06 14:38:27 +0900

    add: MinDeckNumコンポーネントにlocalStrageを導入

--- src/components/parts/initialSetting/MinDeckNum.tsx
+++ src/components/parts/initialSetting/MinDeckNum.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import { Box, Typography, TextField } from "@mui/material";
 
 export default function MinDeckNum(props: any) {
-  const register = props.register;
+  // const register = props.register;
+  const [deckNum, setDeckNum] = useState(0);
+
+  useEffect(() => {
+    // コンポーネントがマウントされた時にlocalStorageから値を読み込む
+    const savedDeckNum = localStorage.getItem("deckNum");
+    if (savedDeckNum) {
+      setDeckNum(Number(savedDeckNum));
+    }
+  }, []);
+
+  const handleDeckNumChange = (event: any) => {
+    const value = event.target.value;
+    setDeckNum(value);
+    // 値が変更されるたびにlocalStorageに保存する
+    localStorage.setItem("deckNum", value);
+  };
 
   return (
     <Box
@@ -21,7 +38,9 @@ export default function MinDeckNum(props: any) {
         id="outlined-basic"
         type="number"
         variant="outlined"
-        {...register("deckNum")}
+        value={deckNum}
+        onChange={handleDeckNumChange}
+        // {...register("deckNum")}
         sx={{
           ml: 2,
         }}

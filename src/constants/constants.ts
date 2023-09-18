15262b4d26ec8f05570e546db9c75794e46036cc
chika <hawmea@yahoo.co.jp>
2023-09-18 14:56:48 +0900

    refactor: 城オブジェクトの日本語プロパティ名を「js」→「ja」に変更

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,7 @@
 export const castelColors = [
-  { js: "赤", en: "red" },
-  { js: "青", en: "blue" },
-  { js: "金", en: "gold" },
+  { ja: "赤", en: "red" },
+  { ja: "青", en: "blue" },
+  { ja: "金", en: "gold" },
 ];
 
 export const castleNumber = [1, 2, 3, 4, 5];

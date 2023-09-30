ba9e5c6ca9b5f40f1078273257bad7df44e9e254
chika <hawmea@yahoo.co.jp>
2023-09-30 13:28:19 +0900

    add: 守る時間と城種別のボタンを追加

--- src/components/pages/Howto.tsx
+++ src/components/pages/Howto.tsx
@@ -1,12 +1,19 @@
+import { useForm } from "react-hook-form";
+import { CalcFormInput } from "../../types";
+
 import Container from "@mui/material/Container";
 import InitialSetting from "../templates/InitialSetting";
+import CalcTime from "../parts/CalcTime";
+import CastelKinds from "../parts/CastelKinds";
 
 export default function Howto() {
   function castleChange() {}
-  const register = "";
+  const { register } = useForm<CalcFormInput>();
   return (
     <Container maxWidth="sm">
       <InitialSetting castleChange={castleChange} register={register} />
+      <CalcTime register={register} />
+      <CastelKinds />
     </Container>
   );
 }

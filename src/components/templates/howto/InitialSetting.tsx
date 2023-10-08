a10608386a71a5939a2482b9c0c3852d3fc9f935
chika <hawmea@yahoo.co.jp>
2023-10-08 12:24:52 +0900

    fix: エラーになるので、まずは複製コンポーネント内で読み込んでいるものの階層を変更

--- src/components/templates/howto/InitialSetting.tsx
+++ src/components/templates/howto/InitialSetting.tsx
@@ -6,9 +6,9 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-import { castelColors } from "../../constants/constants";
-import AlignmentNum from "../parts/initialSetting/AlignmentNum";
-import MinDeckNum from "../parts/initialSetting/MinDeckNum";
+import { castelColors } from "../../../constants/constants";
+import AlignmentNum from "../../parts/initialSetting/AlignmentNum";
+import MinDeckNum from "../../parts/initialSetting/MinDeckNum";
 
 export default function InitialSetting(props: any) {
   const castleChange = props.castleChange;

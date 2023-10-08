855af805d7d613e1307319c90da064e19c2e2090
chika <hawmea@yahoo.co.jp>
2023-10-08 12:24:26 +0900

    add: initialSetting_templateをhowto用に複製

---
+++ src/components/templates/howto/InitialSetting.tsx
@@ -0,0 +1,40 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import { castelColors } from "../../constants/constants";
+import AlignmentNum from "../parts/initialSetting/AlignmentNum";
+import MinDeckNum from "../parts/initialSetting/MinDeckNum";
+
+export default function InitialSetting(props: any) {
+  const castleChange = props.castleChange;
+  const register = props.register;
+
+  return (
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>初期設定</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {castelColors.map((castleColor) => {
+          return (
+            <AlignmentNum
+              castleColorJa={castleColor.ja}
+              castleColorEn={castleColor.en}
+              castleChange={castleChange}
+            />
+          );
+        })}
+        <MinDeckNum register={register} />
+      </AccordionDetails>
+    </Accordion>
+  );
+}

9da406b0d8c18da21dcd7c66dfd7b46c475eca37
chika <hawmea@yahoo.co.jp>
2023-10-09 12:38:14 +0900

    add: 城数の初期設定エリアと一分の駐屯数の設定欄をアコーディオンの中から外にコードを複製

--- src/components/pages/Howto.tsx
+++ src/components/pages/Howto.tsx
@@ -6,12 +6,28 @@ import InitialSetting from "../templates/howto/InitialSetting";
 import CalcTime from "../parts/CalcTime";
 import CastelKinds from "../parts/CastelKinds";
 
+import { castelColors } from "../../constants/constants";
+import AlignmentNum from "../parts/howto/initialSetting/AlignmentNum";
+import MinDeckNum from "../parts/howto/initialSetting/MinDeckNum";
+
 export default function Howto() {
   function castleChange() {}
   const { register } = useForm<CalcFormInput>();
   return (
     <Container maxWidth="sm">
       <InitialSetting castleChange={castleChange} register={register} />
+      <div>
+        {castelColors.map((castleColor) => {
+          return (
+            <AlignmentNum
+              castleColorJa={castleColor.ja}
+              castleColorEn={castleColor.en}
+              castleChange={castleChange}
+            />
+          );
+        })}
+        <MinDeckNum register={register} />
+      </div>
       <CalcTime register={register} />
       <CastelKinds />
     </Container>

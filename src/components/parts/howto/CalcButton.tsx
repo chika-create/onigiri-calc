06afa488f2896ee760d90b1a37d791dfcdc49592
chika <hawmea@yahoo.co.jp>
2023-10-21 11:42:03 +0900

    update: 使い方の「計算する」コンポーネントのロジック部分を削除

--- src/components/parts/howto/CalcButton.tsx
+++ src/components/parts/howto/CalcButton.tsx
@@ -1,13 +1,9 @@
 import { Box, Button } from "@mui/material";
 
-export default function CalcButton(props: any) {
-  const calculator = props.calculator;
-
+export default function CalcButton() {
   return (
     <Box sx={{ textAlign: "center" }}>
-      <Button variant="contained" onClick={calculator}>
-        計算する
-      </Button>
+      <Button variant="contained">計算する</Button>
     </Box>
   );
 }

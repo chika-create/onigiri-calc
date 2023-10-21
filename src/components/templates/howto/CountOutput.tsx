15186bc3f88d2930cf094d2dbf38a959be8a4545
chika <hawmea@yahoo.co.jp>
2023-10-21 12:26:05 +0900

    add: howto用にCountOutputコンポーネントを追加

---
+++ src/components/templates/howto/CountOutput.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+
+import FullStack from "../parts/countOutput/FullStack";
+import FullOnigiri from "../parts/countOutput/FullOnigiri";
+
+export default function CountOutput(props: any) {
+  // クリップボードにコピー
+  async function copyToClipboard(num: any) {
+    try {
+      await navigator.clipboard.writeText(num);
+    } catch (error) {
+      console.log(error || "コピーに失敗しました");
+    }
+  }
+
+  return (
+    <Box
+      sx={{
+        p: 3,
+      }}
+    >
+      <FullOnigiri copyToClipboard={copyToClipboard} />
+      <FullStack copyToClipboard={copyToClipboard} />
+    </Box>
+  );
+}

ac16542a780a3788788c33b365031050bc56e01c
chika <hawmea@yahoo.co.jp>
2023-10-21 15:27:56 +0900

    add: 既存のコードに移設したタブコードを適用

--- src/components/views/AppContent.tsx
+++ src/components/views/AppContent.tsx
@@ -27,36 +27,11 @@ import CalcButton from "../parts/CalcButton";
 import CountOutput from "../templates/CountOutput";
 import TabBox from "./TabBox";
 
-function TabPanel2(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-  return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      id={`tabpanel-${index}`}
-      aria-labelledby={`tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </Box>
-  );
-}
-
 function AppContent() {
-  const [value, setValue] = React.useState("1");
+  const [value, setValue] = useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
-  };
-
-  // タブ管理
-  const [tabValue, setTabValue] = useState(0);
-  const tabChange = (event: SyntheticEvent, newValue: number): void => {
-    setTabValue(newValue);
   };
 
   const {
@@ -75,45 +50,42 @@ function AppContent() {
         <TabContext value={value}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="Item One" value="1" />
-              <Tab label="Item Two" value="2" />
-              <Tab label="Item Three" value="3" />
+              <Tab label="城戦" value="1" />
+              <Tab label="test" value="2" />
+              <Tab label="おにぎり表" value="3" />
             </TabList>
           </Box>
-          <TabPanel value="1">Item One</TabPanel>
-          <TabPanel value="2">Item Two</TabPanel>
-          <TabPanel value="3">Item Three</TabPanel>
-        </TabContext>
 
-        <TabBox tabValue={tabValue} tabChange={tabChange} />
+          <TabPanel value="1" sx={{ p: 0 }}>
+            <InitialSetting castleChange={castleChange} register={register} />
+            <ServerCastle />
 
-        <TabPanel value={tabValue} index={0}>
-          <InitialSetting castleChange={castleChange} register={register} />
-          <ServerCastle />
-
-          <CalcTime register={register} />
-          <CastleKinds onClick={updateSelectCastleKind} />
-          <CalcButton calculator={calculator} />
-          {/* <Box>
+            <CalcTime register={register} />
+            <CastleKinds onClick={updateSelectCastleKind} />
+            <CalcButton calculator={calculator} />
+            {/* <Box>
             <FormControlLabel
               control={<Checkbox />}
               label="今から終了まで（まだ使えません）"
             />
-          </Box> */}
-          <numNumberContext.Provider value={numNumber}>
-            <selectCastleKindContext.Provider value={selectCastleKind}>
-              <alignmentNumbersContext.Provider value={alignmentNum}>
-                <CountOutput numNumber={numNumber} />
-              </alignmentNumbersContext.Provider>
-            </selectCastleKindContext.Provider>
-          </numNumberContext.Provider>
-        </TabPanel>
-        <TabPanel value={tabValue} index={1}>
-          <UniversalCastle />
-        </TabPanel>
-        <TabPanel value={tabValue} index={2}>
-          <OnigiriTable />
-        </TabPanel>
+            </Box> */}
+            <numNumberContext.Provider value={numNumber}>
+              <selectCastleKindContext.Provider value={selectCastleKind}>
+                <alignmentNumbersContext.Provider value={alignmentNum}>
+                  <CountOutput numNumber={numNumber} />
+                </alignmentNumbersContext.Provider>
+              </selectCastleKindContext.Provider>
+            </numNumberContext.Provider>
+          </TabPanel>
+          <TabPanel value="2">
+            <UniversalCastle />
+          </TabPanel>
+          <TabPanel value="3">
+            <OnigiriTable />
+          </TabPanel>
+        </TabContext>
+
+        <TabBox />
       </Box>
     </>
   );

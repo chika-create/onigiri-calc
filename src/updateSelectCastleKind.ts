81d7c5f7cbaf6fc7d7dabf3c35bc6342cde759c0
chika-create <yasui.chika@a-tm.co.jp>
2023-10-26 10:07:51 +0900

    fix: 書き出したupdateSelectCastleKind関数の書き方がおかしかったので修正

--- src/updateSelectCastleKind.ts
+++ src/updateSelectCastleKind.ts
@@ -1,11 +1,16 @@
 import { useState, MouseEvent } from "react";
 
-const [selectCastleKind, setSelectCastleKind] = useState("red");
+function UpdateSelectCastleKind():void {
+  const [selectCastleKind, setSelectCastleKind] = useState("red");
 
-// 選択された城種別を登録し、計算機能を実行
-export const updateSelectCastleKind = (
-  event: MouseEvent<HTMLElement>,
-  newCastleAlignment: string
-): void => {
-  setSelectCastleKind(newCastleAlignment);
-};
+  // 選択された城種別を登録し、計算機能を実行
+  const updateSelectCastleKind2 = (
+    event: MouseEvent<HTMLElement>,
+    newCastleAlignment: string
+  ): void => {
+    setSelectCastleKind(newCastleAlignment);
+  };
+  // return null;
+}
+
+export default UpdateSelectCastleKind;

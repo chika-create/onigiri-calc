e06fedc3ce7a5a1f8a81ea8e1fd1136739e8b165
chika <hawmea@yahoo.co.jp>
2023-11-04 14:26:40 +0900

    refactor: useContextの初期設定を型推論に変更

--- src/SettingUseContext.ts
+++ src/SettingUseContext.ts
@@ -1,11 +1,11 @@
 import { createContext } from "react";
 
-export const stackNumberContext = createContext<number>(0);
-export const alignmentNumContext = createContext<number>(0);
+export const stackNumberContext = createContext(0);
+export const alignmentNumContext = createContext(0);
 export const alignmentNumbersContext = createContext({
   red: 1,
   blue: 2,
   gold: 3,
 });
 
-export const selectCastleKindContext = createContext<string>("red");
+export const selectCastleKindContext = createContext("red");

1ee7bd32d5da7361afe3b052a3e7e185e87c3d1b
chika-create <yasui.chika@a-tm.co.jp>
2024-01-17 09:19:43 +0900

    refactor: 復活させたFooternavのtemplateを呼び出すように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
 import "./App.css";
-import { Breadcrumbs, Box } from "@mui/material";
-
-import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
-import AppContent from "./components/views/AppContent";
-import Term from "./components/pages/Term";
-import PrivacyPolicy from "./components/pages/PrivacyPolicy";
-import { grayColors } from "./constants/colors";
+import FooterNav from "./components/templates/FooteNav";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppContent />} />
-        <Route path="/term" element={<Term />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-      </Routes>
-      <Box sx={{ display: "flex", justifyContent: "center", mb: 2 }}>
-        <Breadcrumbs aria-label="breadcrumb">
-          <Link
-            to="/"
-            style={{ color: grayColors[300], textDecoration: "none" }}
-          >
-            ホーム
-          </Link>
-          <Link
-            to="/term"
-            style={{ color: grayColors[300], textDecoration: "none" }}
-          >
-            利用規約
-          </Link>
-          <Link
-            to="/privacypolicy"
-            style={{ color: grayColors[300], textDecoration: "none" }}
-          >
-            プライバシーポリシー
-          </Link>
-        </Breadcrumbs>
-      </Box>
-    </BrowserRouter>
-  );
+  return <FooterNav />;
 };
 
 export default App;
